3729c68d904dca38823360c8f2023c1063a60f5c
Emre <112270755+emrrekus@users.noreply.github.com>
2024-11-24 00:28:25 +0300

    Entity tanımlandı

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-S74UGVJ;database=TraversalCoreProject;integrated security=true;TrustServerCertificate=True");
+
+        }
+
+        public DbSet<Destination> Destinations { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Guide.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Guide
+    {
+        [Key] 
+        public int GuideId { get; set; }
+        public int Name {  get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+        public bool Status { get; set; }
+
+        public string SocialMedia1 { get; set; }
+        public string SocialMedia2 { get; set; }
+    }
+}
